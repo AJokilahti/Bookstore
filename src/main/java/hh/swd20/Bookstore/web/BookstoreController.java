e1f6b0c5d24d8ab648b2f8dfdcff90ae560e47c7
AJokilahti <aleksi.jokilahti@myy.haaga-helia.fi>
2022-02-16 19:25:55 +0200

    Added book listing

--- src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookstoreController.java
@@ -1,17 +1,29 @@
 package hh.swd20.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.swd20.Bookstore.domain.BookRepository;
+
 @Controller
 public class BookstoreController {
 
+@Autowired
+private BookRepository repository;
+	
 	@RequestMapping(value = "/index", method = RequestMethod.GET)
 	public String Books(Model model) {
-		String hello = "Hello, welcome to the Bookstore. Here are some books:";
-		model.addAttribute("bookslist", hello);
-		return "bookslist";
+		String hello = "Hello, welcome to the Bookstore.";
+		model.addAttribute("index", hello);
+		return "index";
+	}
+	
+	@RequestMapping(value = "/booklist")
+	public String Booklist(Model model) {
+		model.addAttribute("booklist", repository.findAll());
+		return "booklist";
 	}
 }
